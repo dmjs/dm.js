6283e9e71c7a957fff07d5e7240af2dacc6a87ff
Eugene Poltorakov <jslayer@gmail.com>
2013-06-07 16:05:15 +0300

    almost done :)

--- dm.js
+++ dm.js
@@ -1,67 +1,245 @@
-var DM = (function(options) {
+var DM;
+
+function DMModule(callback, context) {
+  this.body = callback;
+  this.bodyContext = context;
+}
+
+DMModule.STATES = {
+  INITIAL  : 0,
+  BEFORE   : 1,
+  BODY     : 2,
+  AFTER    : 3,
+  FINISHED : 4
+};
+
+DMModule.TYPES = {
+  CONTINUE : 'continue',
+  STOP     : 'stop'
+};
+
+DMModule.prototype = {
+  constructor : DMModule,
+  _before     : [],
+  _after      : [],
+  context     : null,
+  body        : null,
+  bodyContext : null,
+  state       : 0,
+  index       : 0,
+  sorted      : false,
+  _sort       : function(arr) {
+    arr.sort(function(a,b) {
+      var result;
+
+      if (a.weight === b.weight) {
+        result = 0;
+      }
+      else {
+        result = a.weight > b.weight ? 1 : -1;
+      }
+
+      return result;
+    });
+  },
+  before   : function(callback, context, weight){
+    this.sorted = false;
+
+    this._before.push({
+      callback : callback,
+      context  : context,
+      weight   : weight || 0
+    });
+
+    return this;
+  },
+  after    : function(callback, context, weight){
+    this.sorted = false;
+
+    this._after.push({
+      callback : callback,
+      context  : context,
+      weight   : weight || 0
+    });
+
+    return this;
+  },
+  /**
+   *
+   * @param {String} type
+   * @returns {*}
+   */
+  execute  : function(type){
+    var states = DMModule.STATES,
+      types = DMModule.TYPES,
+      obj,
+      result;
+
+    if (!this.sorted) {
+      this._sort(this._after);
+      this._sort(this._before);
+      this.sorted = true;
+    }
+
+    switch (type) {
+      case types.CONTINUE:
+
+        break;
+    }
+
+    if (type === types.CONTINUE && this.state === states.INITIAL) {
+      //do nothing when continue executed in INITIAL state
+      return;
+    }
+
+    //noinspection FallthroughInSwitchStatementJS
+    switch(this.state) {
+      case states.INITIAL:
+        this.state = states.BEFORE;
+      case states.BEFORE:
+        obj = this._before[this.index];
+        if (obj && typeof obj.callback === 'function') {
+          this.context = obj.context;
+          result = obj.callback.apply(this);
+        }
+        else {
+          this.state = states.BODY;
+          this.index = 0;
+          this.continue();
+        }
+        break;
+      case states.BODY:
+        if (typeof this.body === 'function') {
+          this.context = this.bodyContext;
+          this.body.apply(this);
+          this.state = states.AFTER;
+          this.index = -1;
+          result = true;
+        }
+        break;
+      case states.AFTER:
+        obj = this._after[this.index];
+        if (obj && typeof obj.callback === 'function') {
+          this.context = obj.context;
+          result = obj.callback.apply(this);
+        }
+        else {
+          this.state = states.FINISHED;
+          this.index = 0;
+          this.continue();
+        }
+        break;
+      case states.FINISHED:
+        this.state = DMModule.STATES.INITIAL;
+        break;
+    }
+
+    switch(result) {
+      case true:
+        this.continue();
+        break;
+      case false:
+        this.stop();
+        break;
+    }
+
+    return this;
+  },
+  continue : function(){
+    this.index++;
+    this.execute(DMModule.TYPES.CONTINUE);
+  },
+  stop     : function(){
+    this.index = 0;
+    this.state = DMModule.STATES.FINISHED;
+    this.execute(DMModule.TYPES.STOP);
+  }
+};
+
+DMUtils = {
+  /**
+   * Each utility function
+   * @param {Object} obj
+   * @param {Function} callback
+   * @param {Object?} context
+   */
+  each : function(obj, callback, context) {
+    var i;
+
+    for(i in obj) {
+      if (obj.hasOwnProperty(i)) {
+        callback.call(context, obj[i], i);
+      }
+    }
+  }
+};
+
+
+DM = (function(options) {
   var _modules,
-    udf;
+    UNDEFINED = 'undefined';
 
-  console.log(options);
-
-  function Module(callback, context) {
-    this.body = callback;
-    this.context = context;
+  /**
+   * Create new {DMModule} instance
+   * @param {string} name
+   * @param {Function?} callback
+   * @param {Object?} context
+   * @returns {DMModule}
+   */
+  function createModule(name, callback, context) {
+    return new DMModule(callback, context);
   }
 
-  Module.STATES = {
-    INITIAL : 0,
-    STARTED : 1,
-    BEFORE  : 2,
-    BODY    : 3,
-    AFTER   : 4
-  };
+  /**
+   * Get existing module or false
+   * @param {String} name
+   * @returns {DMModule|Boolean}
+   */
+  function getModule(name) {
+    var module = _modules[name];
 
-  Module.prototype._before = {};
-  Module.prototype._after = {};
-  //Module.prototype._callback = null;
-  Module.prototype.context = null;
-  Module.prototype._state = 0;
-
-  Module.prototype.before = function(callback, context) {};
-
-  Module.prototype.after = function(callback, context) {};
-
-  Module.prototype.body = function(callback, context) {};
-
-  Module.prototype.execute = function() {
-    //console.log(this);
-    if(typeof(this.body) === 'function') {
-      this.body.call(this.context);
-    }
-  };
-
-  Module.prototype.continue = function() {};
-
-  Module.prototype.stop = function() {};
+    return module instanceof DMModule ? module : false;
+  }
 
   _modules = {};
 
   return {
     add : function(name, callback, context) {
-      if (typeof _modules[name] !== 'undefined') {
+      if (getModule(name)) {
         throw new Error('Module `' + name + '` is already defined');
       }
-      _modules[name] = new Module(callback, context);
-    },
-    before : function(name, callback, context) {
+      _modules[name] = createModule(name, callback, context);
 
+      return this;
     },
-    after : function(name, callback, context) {
+    before : function(name, callback, context, weight) {
+      var module = getModule(name) || createModule(name);
 
+      if (typeof callback !== 'function') {
+        throw new Error('Callback should be a function');
+      }
+
+      module.before(callback, context, weight);
+
+      return this;
+    },
+    after : function(name, callback, context, weight) {
+      var module = getModule(name) || createModule(name);
+
+      if (typeof callback !== 'function') {
+        throw new Error('Callback should be a function');
+      }
+
+      module.after(callback, context, weight);
+
+      return this;
     },
     go : function() {
-      //console.log(_modules);
-      for(i in _modules) {
-        if (_modules.hasOwnProperty(i)) {
-          _modules[i].execute();
-        }
-      }
+      DMUtils.each(_modules, function(module) {
+        module.execute();
+      });
+
+      return this;
     }
   };
 })({
@@ -74,13 +252,3 @@ var DM = (function(options) {
     y : typeof YUI === 'function' && YUI
   }
 });
-
-DM.add('test', function(){
-  console.group('test module');
-  console.log(arguments, this);
-  console.groupEnd('test module');
-}, {
-  a: 1
-});
-
-DM.go();
