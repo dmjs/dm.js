d86f83b21c0f69e0f749a87655fa8c355a9af9e6
Eugene Poltorakov <jslayer@gmail.com>
2013-06-13 12:37:21 +0300

    start working on 0.2 v

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,14 @@
 #Changelog
 
+## 0.2.0 (Jun 13 2013)
+* changed: the modules worlflow. DMExec doesn't look at the callback return value since now
+* changed : `before/after/add` now returns the uuid of the callback;
+* added : `DMExec.wait([timeout],[next = false])`
+* change : name of the intermal DMExec properties
+* added : `DM.remove([before/after/add uuid])`
+* added : `DM.clean(name)`
+* added : `DM.cleanAll()`
+
 ## 0.1.1 (Jun 11 2013)
 * changed: [README][readme]
 * changed: demos
--- dm.js
+++ dm.js
@@ -20,7 +20,6 @@ DMUtils = {
     return arr.map(callback, context);
   },
   /**
-   * @param {Function} callback
    * @param {String} selector
    * @param {HTMLElement|HTMLDocument|?} ctx
    * @returns {NodeList}
@@ -86,75 +85,113 @@ DMUtils = {
       }
       return result;
     });
-  }
+  },
+  /**
+   * @returns {Function}
+   */
+  uuid : (function() {
+    var uuid = 0;
+    return function() {
+      return ++uuid;
+    };
+  })()
 };
 
+/**
+ *
+ * @param {Function?} callback
+ * @param {*?} context
+ * @constructor
+ */
 function DMModule(callback, context) {
-  this.body = callback;
-  this.context = context;
+  this.uuid = DMUtils.uuid();
+
+  this._before = [];
+  this._after = [];
+  this._add = {
+    callback : callback,
+    context  : context
+  };
+  this.ready = false;
 }
 
-DMModule.prototype = {
-  constructor : DMModule,
-  _before     : [],
-  _after      : [],
-  body        : null,
-  context     : null,
-  ready       : false,
-  _sort       : function(arr){
-    arr.sort(function(a,b) {
-      var result;
+DMModule.prototype._sort = function(arr){
+  arr.sort(function(a, b){
+    var result;
 
-      if (a.weight === b.weight) {
-        result = 0;
-      }
-      else {
-        result = a.weight > b.weight ? 1 : -1;
-      }
+    if (a.weight === b.weight) {
+      result = 0;
+    }
+    else {
+      result = a.weight > b.weight ? 1 : -1;
+    }
 
-      return result;
-    });
-    return this;
-  },
-  prepare : function() {
-    this._sort(this._after)._sort(this._before);
-    this.ready = true;
-    return this;
-  },
-  before   : function(callback, context, weight){
-    this.ready = false;
+    return result;
+  });
+  return this;
+};
+DMModule.prototype.prepare = function(){
+  this._sort(this._before)._sort(this._after);
+  this.ready = true;
+  return this;
+};
+/**
+ * @param {Function} callback
+ * @param {*?} context
+ * @param {Number?} weight
+ * @returns {Number}
+ */
+DMModule.prototype.before = function(callback, context, weight){
+  var id = DMUtils.uuid();
 
-    this._before.push({
-      callback : callback,
-      context  : context,
-      weight   : weight || 0
-    });
+  this.ready = false;
 
-    return this;
-  },
-  after    : function(callback, context, weight){
-    this.ready = false;
+  this._before.push({
+    callback : callback,
+    context  : context,
+    weight   : weight || 0,
+    uuid     : id
+  });
 
-    this._after.push({
-      callback : callback,
-      context  : context,
-      weight   : weight || 0
-    });
+  return id;
+};
+/**
+ * @param {Function} callback
+ * @param {*?} context
+ * @param {Number?} weight
+ * @returns {Number}
+ */
+DMModule.prototype.after = function(callback, context, weight){
+  var id = DMUtils.uuid();
 
-    return this;
-  }
+  this.ready = false;
+
+  this._after.push({
+    callback : callback,
+    context  : context,
+    weight   : weight || 0,
+    uuid     : id
+  });
+
+  return id;
 };
 
 function DMExec(module, node, args) {
   this.module = module;
   this.node = node;
   this.args = args;
+
+  this._state = 0;
+  this._index = 0;
+  this.context = null;
+  this._waiting = null;
+  this._timer = null;
 }
 
 DMExec.STATES = {
   INITIAL  : 0,
   BEFORE   : 1,
-  BODY     : 2,
+  MAIN     : 2,
   AFTER    : 3,
   FINISHED : 4
 };
@@ -164,94 +201,95 @@ DMExec.TYPES = {
   STOP : 'stop'
 };
 
-DMExec.prototype = {
-  constructor : DMExec,
-  state       : 0,
-  index       : 0,
-  module      : null,
-  node        : null,
-  args        : null,
-  context     : null,
-  next : function(){
-    this.index++;
-    this.execute(DMExec.TYPES.NEXT);
-  },
-  stop : function(){
-    this.index = 0;
-    this.state = DMExec.STATES.FINISHED;
-    this.execute(DMExec.TYPES.STOP);
-  },
-  /**
-   *
-   * @param {String?} type
-   * @returns {*}
-   */
-  execute : function(type) {
-    var states = DMExec.STATES,
-      types = DMExec.TYPES,
-      module = this.module,
-      obj,
-      nextState,
-      result;
-
-    if (!(type === types.NEXT && this.state === states.INITIAL)) {
-      if (!module.ready) {
-        module.prepare();
-      }
-    }
-
-    if (this.state === states.INITIAL) {
-      this.state = states.BEFORE;
-    }
-
-    //noinspection FallthroughInSwitchStatementJS
-    switch (this.state) {
-      case states.BEFORE:
-      case states.AFTER:
-        obj = module[this.state === states.BEFORE ? '_before' : '_after'][this.index];
-
-        if (obj && typeof obj.callback === 'function') {
-          this.context = obj.context;
-          result = obj.callback.apply(this, this.args);
-        }
-        else {
-          this.state = this.state === states.BEFORE ? states.BODY : states.FINISHED;
-          this.index = -1;
-          this.next();
-        }
-        break;
-      case states.BODY:
-        if (typeof module.body === 'function') {
-          this.context = module.context;
-          result = module.body.apply(this, this.args);
-          this.state = states.AFTER;
-          this.index = -1;
-        }
-        break;
-      case states.FINISHED:
-        this.state = states.INITIAL;
-        this.index = 0;
-        this.context = null;
-        break;
-    }
-
-    if (this.state !== states.INITIAL) {
-      if (result === false) {
-        this.stop();
-      }
-      else if (result) {
-        this.next();
-      }
-    }
-
-    return this;
+DMExec.prototype.next = function(){
+  if (this._waiting) {
+    this._waiting = false;
+    clearTimeout(this._timer);
   }
+  this._index++;
+  this.execute(DMExec.TYPES.NEXT);
+};
+DMExec.prototype.stop = function(){
+  if (this._waiting) {
+    this._waiting = false;
+    clearTimeout(this._timer);
+  }
+  this._index = 0;
+  this._state = DMExec.STATES.FINISHED;
+  this.execute(DMExec.TYPES.STOP);
+};
+/**
+ *
+ * @param {String?} type
+ * @returns {*}
+ */
+DMExec.prototype.execute = function(type){
+  var states = DMExec.STATES,
+    types = DMExec.TYPES,
+    module = this.module,
+    obj;
+
+  if (!(type === types.NEXT && this._state === states.INITIAL)) {
+    if (!module.ready) {
+      module.prepare();
+    }
+  }
+
+  if (this._state === states.INITIAL) {
+    this._state = states.BEFORE;
+  }
+
+  //noinspection FallthroughInSwitchStatementJS
+  switch (this._state) {
+    case states.BEFORE:
+    case states.AFTER:
+      obj = module[this._state === states.BEFORE ? '_before' : '_after'][this._index];
+
+      if (obj && typeof obj.callback === 'function') {
+        this.context = obj.context;
+        obj.callback.apply(this, this.args);
+      }
+      else {
+        this._state = this._state === states.BEFORE ? states.MAIN : states.FINISHED;
+        this._index = -1;
+        //this.next();
+      }
+      break;
+    case states.MAIN:
+      if (typeof module._add.callback === 'function') {
+        this.context = module._add.context;
+        module._add.callback.apply(this, this.args);
+      }
+      this._state = states.AFTER;
+      this._index = -1;
+      break;
+    case states.FINISHED:
+      this._state = states.INITIAL;
+      this._index = 0;
+      this.context = null;
+      break;
+  }
+
+  if (this._state !== states.INITIAL && !this._waiting) {
+    this.next();
+  }
+
+  return this;
+};
+DMExec.prototype.wait = function(timeout, next){
+  var self = this;
+
+  this._waiting = true;
+
+  this._timer = setTimeout(function(){
+    self[next ? 'next' : 'stop']();
+  }, timeout || 5000);
 };
 
+
 DM = (function(options) {
-  var _modules,
-    _engine,
-    UNDEFINED = 'undefined';
+  var _modules = {},
+    _engine;
 
   function initEngine(callback) {
     if (!_engine) {
@@ -332,22 +370,23 @@ DM = (function(options) {
     return module instanceof DMModule ? module : false;
   }
 
-  _modules = {};
-
   return {
     add : function(name, callback, context) {
       var module = getModule(name);
       if (module) {
-        if (typeof module.body === 'function') {
-          throw new Error('Module(' + name + ') body callback is already defined');
+        if (typeof module._add.callback === 'function') {
+          throw new Error('Module(' + name + ') main callback is already defined');
         }
         else {
-          module.body = callback;
+          module._add.callback = callback;
+          module._add.context = context;
         }
       }
-      createModule(name, callback, context);
+      else {
+        module = createModule(name, callback, context);
+      }
 
-      return this;
+      return module.uuid;
     },
     before : function(name, callback, context, weight) {
       var module = getModule(name) || createModule(name);
@@ -356,9 +395,7 @@ DM = (function(options) {
         throw new Error('Callback should be a function');
       }
 
-      module.before(callback, context, weight);
-
-      return this;
+      return module.before(callback, context, weight);
     },
     after : function(name, callback, context, weight) {
       var module = getModule(name) || createModule(name);
@@ -367,9 +404,7 @@ DM = (function(options) {
         throw new Error('Callback should be a function');
       }
 
-      module.after(callback, context, weight);
-
-      return this;
+      return module.after(callback, context, weight);
     },
     go : function() {
       initEngine(function() {
@@ -389,6 +424,19 @@ DM = (function(options) {
           });
         });
       });
+      return this;
+    },
+    remove : function(uuid) {
+
+    },
+    clean : function(name) {
+
+    },
+    /**
+     * Remove all modules;
+     */
+    cleanAll : function() {
+      _modules = {};
       return this;
     }
   };
