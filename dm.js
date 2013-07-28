89adcb794600bd3804f878a436b24b86c1cd1a41
Eugene Poltorakov <jslayer@gmail.com>
2013-07-28 13:28:28 +0300

    Update dm.js

--- dm.js
+++ dm.js
@@ -6,6 +6,7 @@
 
 var DM, DMUtils;
 //TODO - outer dependencies support (addDep, removeDep, getDep)
+//TODO - implement registry retriving (for debug)
 DMUtils = {
     /**
      * Each utility function
