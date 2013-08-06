9bd6c562395281fc441baf7a1de6e601cdd8ca7c
Eugene Poltorakov <jslayer@gmail.com>
2013-08-07 02:27:02 +0300

    upload docs

---
+++ docs/out/api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "DM",
+        "DMExec",
+        "DMModule"
+    ],
+    "modules": [
+        "dm"
+    ],
+    "allModules": [
+        {
+            "displayName": "dm",
+            "name": "dm",
+            "description": "DOM Markers"
+        }
+    ]
+} };
+});
